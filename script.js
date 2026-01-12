1be0693f856bc5f01f09a851a47990b91abba2e5
7674886 <7674886@gmail.com>
2026-01-12 10:55:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-01
+const text = document.getElementById("text");
+const preview = document.getElementById("preview");
+const font = document.getElementById("font");
+
+text.addEventListener("input", () => {
+  preview.textContent = text.value;
+});
+
+font.addEventListener("change", () => {
+  preview.style.fontFamily = font.value;
+});
